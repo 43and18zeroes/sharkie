bec4c640632f143e8474ae1c4368f37dc8a60655
Christoph <c.wagner.germany@gmail.com>
2022-04-22 18:49:35 +0200

    added enemies

--- models/pufferfish.class.js
+++ models/pufferfish.class.js
@@ -2,6 +2,8 @@ class Pufferfish extends MovableObject {
 
     constructor() {
         super().loadImage('img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
+
+        this.x = 300 + Math.random() * 300; // Immer Zahl zwischen 300 und 600
     }
 
 
--- models/world.class.js
+++ models/world.class.js
@@ -7,15 +7,40 @@ class World {
         new Pufferfish(),
         new Pufferfish()
     ];
+    // Hilfsvariable canvas, da clearRect ansonsten nicht auf
+    // width and height zugreifen kann
+    canvas;
     ctx;
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        // Hilfsvariable weiter oben
+        this.canvas = canvas;
         this.draw();
     }
 
     draw() {
+        // Damit beim fortwährenden Aufruf der draw() Funktion (siehe unten)
+        // die zuvorigen Frames gecleart werden:
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         // Die letzten zwei Argumente geben die Größe des Bilds an
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+
+        // Um auch die enemies erneut zu zeichnen wird mit einer forEach-Schleife
+        // auf das Feld zugegriffen
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        })
+
+        // draw() wird durch requestAnimationFrame immer wieder aufgerufen,
+        // so oft es die Grafikkarte hergibt
+        // Da bei requestAnimationFrame die Funktion asynchron etwas später
+        // ausgeführt wird, funktioniert das this nicht mehr. Als workaround
+        // wird das this in eine Variable gepackt.
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        })
     }
 }
